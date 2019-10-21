d14fea29d2d7d0635d63ebc6e0702f7d6b204f76
Tasnim Ahmed <phonedev@tazzix.com>
2019-10-21 18:04:08 +0500

    Progressive web app variant, might be a good idea to switch from localstorage to indexDB

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,20 @@
 //========================================================================================
+// == Service Worker registration for PWA
+//========================================================================================
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js', {
+      scope: '/' // <--- THIS BIT IS REQUIRED
+  }).then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+  });
+}
+
+//========================================================================================
 // == Code for soft keys
 //========================================================================================
 
@@ -267,3 +283,96 @@ document.addEventListener("keydown", event => {
       return;
   }
 });
+
+
+
+//========================================================================================
+// == Key handling on keyboard
+//========================================================================================
+
+/**
+ * Method for sending to-do item to API
+ * /
+function sendTaskToAPI(item, callback) {
+  var req = new XMLHttpRequest();
+  req.open('POST', '/tasks/add');
+  req.setRequestHeader('Content-Type', 'application/json');
+  req.send(JSON.stringify({ item: item }));
+
+  req.addEventListener('load', () => {
+    var results = JSON.parse(req.responseText);
+    if (results.error) return console.log(results.error);
+
+    if (callback) callback(results);
+  });
+
+  req.addEventListener('error', (e) => {
+    console.log('Shit, something bad happened.');
+    console.log(e);
+  });
+}
+*/
+/**
+ * Will fetch all tasks from API.
+ * /
+function getTasks(callback) {
+  var req = new XMLHttpRequest();
+  req.open('GET', '/tasks');
+  req.send();
+
+  req.addEventListener('load', () => {
+    var results = JSON.parse(req.responseText);
+    if (results.error) return console.log(results.error);
+
+    if (callback) callback(results);
+  });
+
+  req.addEventListener('error', (e) => {
+    console.log('Shit, something bad happened.');
+    console.log(e);
+  });
+}
+*/
+
+/*
+const app = document.getElementById('root')
+
+const logo = document.createElement('img')
+logo.src = 'logo.png'
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(logo)
+app.appendChild(container)
+
+var request = new XMLHttpRequest()
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+
+      const h1 = document.createElement('h1')
+      h1.textContent = movie.title
+
+      const p = document.createElement('p')
+      movie.description = movie.description.substring(0, 300)
+      p.textContent = `${movie.description}...`
+
+      container.appendChild(card)
+      card.appendChild(h1)
+      card.appendChild(p)
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+  }
+}
+
+request.send()
+*/
